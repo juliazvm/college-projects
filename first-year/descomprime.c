76fea7ea0ad785c3e2026f110df0d558c044db47
Julia Mendonca <juliaazvm06@proton.me>
2026-07-09 16:06:30 -0300

    Update and categorize file as part of subgroup first-year

--- first-year/descomprime.c
+++ first-year/descomprime.c
@@ -1,19 +1,6 @@
 /******************************************************************************
-Ao preencher esse cabeçalho com o meu nome e o meu número USP, declaro que sou
-o único autor e responsável por esse programa. Todas as partes originais desse
-Exercício-Programa (EP) foram desenvolvidas e implementadas por mim seguindo
-as instruções desse EP e que portanto não constituem desonestidade acadêmica
-ou plágio.
-Declaro também que sou responsável por todas as cópias desse programa e que eu
-não distribuí ou facilitei a sua distribuição. Estou ciente que os casos de
-plágio e desonestidade acadêmica serão tratados segundo os critérios
-divulgados na página da disciplina.
-Entendo que EPs sem assinatura não serão corrigidos e, ainda assim, poderão
-ser punidos por desonestidade acadêmica.
-Nome : Julia de Azevedo Mendonca 
-NUSP :17869668
-Turma:01
-Prof.: Yoshi
+Um decompressor, programa que recebe números gerados pelo primeiro programa e
+visualiza-os, produzindo uma imagem em PNG.
 ******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
