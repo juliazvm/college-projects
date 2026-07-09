4618c6752e10778e45111b358eaff94dc9e34353
Julia Mendonca <juliaazvm06@proton.me>
2026-07-09 16:05:20 -0300

    Update and rename categorize file as part of subgroup first-year

--- first-year/comprime.c
+++ first-year/comprime.c
@@ -1,19 +1,8 @@
 /******************************************************************************
-Ao preencher esse cabeçalho com o meu nome e o meu número USP, declaro que sou
-o único autor e responsável por esse programa. Todas as partes originais desse
-Exercício-Programa (EP) foram desenvolvidas e implementadas por mim seguindo
-as instruções desse EP e que portanto não constituem desonestidade acadêmica
-ou plágio.
-Declaro também que sou responsável por todas as cópias desse programa e que eu
-não distribuí ou facilitei a sua distribuição. Estou ciente que os casos de
-plágio e desonestidade acadêmica serão tratados segundo os critérios
-divulgados na página da disciplina.
-Entendo que EPs sem assinatura não serão corrigidos e, ainda assim, poderão
-ser punidos por desonestidade acadêmica.
-Nome : Julia de Azevedo Mendonca 
-NUSP : 17869668
-Turma: 01
-Prof.: Yoshi
+Um compressor, programa que lê uma imagem e imprime números que representam a
+imagem compactamente. A compressão mudará a imagem, mas apenas de modos quase
+imperceptíveis. A saída consiste na impressão de números representantes da imagem 
+codificada.
 ******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
